9ae9f903a28a0d3c053ce16243f978b121a888a9
Lu Han <lh563@mail.ubc.ca>
2018-11-08 10:36:25 -0800

    final version

---
+++ cm105/powers/R/square.R
@@ -0,0 +1,18 @@
+#' @title Square the input
+#' @param x Vector of numerics
+#' @return The vector x, squared.
+#' @export
+square <- function(x) {x^2
+
+}
+
+pow <- function(x, a=2) x^a
+
+#' @title cube the input
+#' @param x Vector of numerics
+#' @return The vector x, cube.
+#' @export
+cube <- function(x) {
+  pow(x,a=3)
+
+}
